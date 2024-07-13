e4aacb97af9ddafbb3042ca7273c424673020e29
Snappy678 <lester1liau@gmail.com>
2024-07-13 18:59:02 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Pet Haven</title>
+       <h1>Welcome to Pet Haven</h1>
+<ul class="nav justify-content-center, nav-underline">
+         <li class="nav-item">
+             <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+         </li>
+           <li class="nav-item">
+                <a class="nav-link" href="#">About Us</a>
+           </li>
+             <li class="nav-item">
+                  <a class="nav-link" href="#">Pets</a>
+             </li>
+               <li class="nav-item">
+                    <a class="nav-link" href="#">Dogs</a>
+               </li>
+                 <li class="nav-item">
+                    <a class="nav-link" href="#">Cats</a>
+                 </li>
+</ul>    
+    </head>
+    <body>
+ 
+       <?php
+          
+        ?>
+    </body>
+</html>
