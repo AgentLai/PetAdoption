6656fbad51e1a714ec69957a0c510b5e02d941bd
Snappy678 <lester1liau@gmail.com>
2024-07-14 10:47:59 +0800

    index.php
    updated frfr

--- index.php
+++ index.php
@@ -4,31 +4,22 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<html>
+<html lang="en">
     <head>
         <meta charset="UTF-8">
         <title>Pet Haven</title>
-       <h1>Welcome to Pet Haven</h1>
-<ul class="nav justify-content-center, nav-underline">
-         <li class="nav-item">
-             <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-         </li>
-           <li class="nav-item">
-                <a class="nav-link" href="#">About Us</a>
-           </li>
-             <li class="nav-item">
-                  <a class="nav-link" href="#">Pets</a>
-             </li>
-               <li class="nav-item">
-                    <a class="nav-link" href="#">Dogs</a>
-               </li>
-                 <li class="nav-item">
-                    <a class="nav-link" href="#">Cats</a>
-                 </li>
-</ul>    
+        <link rel="stylesheet" href="style.css">
     </head>
     <body>
- 
+      <h1>Welcome to Pet Haven</h1>  
+      <nav class="navbar">    
+          <ul>
+              <li><a href="index.php">Home</a></li>
+              <li><a href="AboutUs.php">About Us</a></li>
+              <li><a href="Cats.php">Cats</a></li>
+              <li><a href="Dogs.php">Dogs</a></li>
+          </ul>
+      </nav> 
        <?php
           
         ?>
