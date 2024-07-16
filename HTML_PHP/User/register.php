28a456d6ce05aed59db3ff6e66a11faa9f2b6ef2
AgentLai <Agentlaiyishen@gmail.com>
2024-07-16 19:46:43 +0800

    Added more files and admin file

---
+++ HTML_PHP/User/register.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+    <meta charset = "UTF-8">
+    <meta http-equiv = "X-UA-Compatible" content = "IE-edge">
+    <meta name = "viewpoint" content="width=device-width, initial-scale = 1.0">
+    <title>Registration Form </title>
+    <link href = "" rel ="">
+</head>
+<body>
+    <div class="py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="register-card">
+                        <div class="register-card-header">
+                            <h5>Registration Form</h5>
+                        </div>
+                        <div class="login-card-body">
+                            <form>
+                                <input type="email" placeholder="Email" class="input-field" required>
+                                <input type="number" placeholder="Phone Number" class ="input-field" reqiured>
+                                <input type="password" placeholder="Password" maxlength="8" class="input-field" required>
+                                <input type="password" placeholder="Confirm Password" maxlength="8" class="input-field" required>
+                                <button>Register Your Account</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
