4a1a77e06f3ae82faeeb5353fa01478a82b6325b
EdwinChin2309 <edwinccw2309@gmail.com>
2024-07-17 14:29:40 +0800

    Added new files

--- HTML_PHP/User/login.php
+++ HTML_PHP/User/login.php
@@ -1,33 +1,64 @@
+<?php
+    session_start();
+?>
+
 <!DOCTYPE html>
-<html lang = "en">
+<html lang="en">
 <head>
-    <meta charset = "UTF-8">
-    <meta http-equiv = "X-UA-Compatible" content = "IE-edge">
-    <meta name = "viewpoint" content="width=device-width, initial-scale = 1.0">
-    <title>Login Form </title>
-    <link href = "" rel ="">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Login</title>
 </head>
 <body>
-    <div class="py-5">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="login-card">
-                        <div class="login-card-header">
-                            <h5>Login Form</h5>
-                        </div>
-                        <div class="login-card-body">
-                            <form>
-                                <input type="email" placeholder="Email" class="input-field" required>
-                                <input type="password" placeholder="Password" maxlength="8" class="input-field" required>
-                                <a href="#">Forgot Your Password?</a>
-                                <button>Sign In</button>
-                            </form>
-                        </div>
-                    </div>
+    <div class="container">
+        <div class="box form-box">
+            <?php
+                include("php/config.php");
+                if(isset($_POST['submit'])){
+                    $email = mysqli_real_escape_string($con,$_POST['email']);
+                    $password = mysqli_real_escape_string($con,$_POST['password']);
+
+                    $result = mysqli_query($con,"SELECT * FROM member WHERE Email='$email' AND Password = '$password'") or die("Selection Error");
+                    $row = mysqli_fetch_assoc($result);
+
+                    if(is_array($row) && !empty($row)){
+                        $_SESSION['valid'] = $row['Email'];
+                        $_SESSION['username'] = $row['Username'];
+                        $_SESSION['dob'] = $row['DoB'];
+                        $_SESSION['memberID'] = $row['memberID'];
+                    } else{
+                        echo "<div class = 'message'>
+                            <p> Wrong Username or Password </p>
+                            </div> <br>";
+                        echo"< a href = 'index.php'><button class = 'btn'> Go Back </button>";
+                    }
+                    if(isset($_SESSION['valid'])){
+                        header("Location: login.php")
+                    }
+                }else{
+                
+            ?>
+            <header>Login</header>
+            <form action="" method="post">
+                <div class="field input">
+                    <label for="email">Email</label>
+                    <input type="text" name="username" id="username" autocomplete="off"  required>
                 </div>
-            </div>
+                <div class="field input">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password" autocomplete="off" required>
+                </div>
+                <div class="field">
+                    <input type="submit" class="btn" name="submit" value="Login" required>
+                </div>
+                <div class="links">
+                    Don't have an account? <a href="register.php">Register One Now!</a>
+                </div>
+            </form>
         </div>
+        <?php } ?>
     </div>
+    
 </body>
 </html>
--- HTML_PHP/User/register.php
+++ HTML_PHP/User/register.php
@@ -1,34 +1,81 @@
+<?php
+    session_start();
+?>
+
 <!DOCTYPE html>
-<html lang = "en">
+<html lang="en">
 <head>
-    <meta charset = "UTF-8">
-    <meta http-equiv = "X-UA-Compatible" content = "IE-edge">
-    <meta name = "viewpoint" content="width=device-width, initial-scale = 1.0">
-    <title>Registration Form </title>
-    <link href = "" rel ="">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Register</title>
 </head>
 <body>
-    <div class="py-5">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="register-card">
-                        <div class="register-card-header">
-                            <h5>Registration Form</h5>
-                        </div>
-                        <div class="login-card-body">
-                            <form>
-                                <input type="email" placeholder="Email" class="input-field" required>
-                                <input type="number" placeholder="Phone Number" class ="input-field" reqiured>
-                                <input type="password" placeholder="Password" maxlength="8" class="input-field" required>
-                                <input type="password" placeholder="Confirm Password" maxlength="8" class="input-field" required>
-                                <button>Register Your Account</button>
-                            </form>
-                        </div>
-                    </div>
+    <div class="container">
+        <div class="box form-box">
+
+        <?php
+            include("php/config.php");
+            if(isset($_POST['submit'])){
+
+                $firstName = $_POST['firstName'];
+                $lastName = $_POST['lastName'];
+                $dob = $_POST['dob'];
+                $email = $_POST['email'];
+                $password = $_POST['password'];
+            $verify_query = mysqli_query($con, "SELECT Email FROM member WHERE Email = '$email'");
+            if(mysqli_num_rows($verify_query)!=0){
+                echo"<div class = 'message'>
+                     <p> This email is in use, please try again </p>
+                    </div> <br>";
+                echo "<a href='javascript:self:histoy:back()'><button class = 'btn'> Go Back </button>";
+            }
+            else{
+                mysqli_query($con, "INSERT INTO member(firstName, lastName, Email, DOB, Password) VALUES('$firstName','$lastName','$dob','$email','$password')") or die("Error Occured");
+                echo"<div class = 'message'>
+                        <p> Registration Success! </p>
+                    </div> <br>";
+                echo "<a href='index.php'><button class = 'btn'> Go Back </button>";
+            }
+        }else{
+        ?>
+            <header>Sign Up</header>
+            <form action="" method="post">
+                <div class="field input">
+                    <label for="firstName">First Name</label>
+                    <input type="text" name="firstName" id="firstName"  required>
                 </div>
-            </div>
+                <div class="field input">
+                    <label for="lastName">Last Name</label>
+                    <input type="text" name="lastName" id="lastName" required>
+                </div>
+                <div class="field input">
+                    <label for="dob">Date Of Birth</label>
+                    <input type="date" name="dob" id="dob" required>
+                </div>
+                <div class="field input">
+                    <label for="email">Email</label>
+                    <input type="email" name="email" id="email" required>
+                </div>
+                <div class="field input">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password" required>
+                </div>
+                <div class="field input">
+                    <label for="password">Confirm Password</label>
+                    <input type="cpassword" name="password" id="cpassword" required>
+                </div>
+                <div class="field">
+                    <input type="submit" class="btn" name="submit" value="Sign Up" required>
+                </div>
+                <div class="links">
+                    Already a member? <a href="login.php">Login Now!</a>
+                </div>
+            </form>
         </div>
+        <?php } ?>
     </div>
+    
 </body>
+</html>
 </html>
