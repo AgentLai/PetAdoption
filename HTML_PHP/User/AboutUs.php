3338765d1e5f1cc3c7c0f62f44e4184aa624b4f8
Snappy678 <lester1liau@gmail.com>
2024-07-23 13:02:39 +0800

    AboutUs.php
    1.1 About us

--- HTML_PHP/User/AboutUs.php
+++ HTML_PHP/User/AboutUs.php
@@ -1,8 +1,40 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Pet Haven</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+     <nav class="navbar">    
+          <ul>
+              <li><a href="index.php">Home</a></li>
+              <li><a href="AboutUs.php">About Us</a></li>
+              <div class="dropdown">
+                <button class="dropbtn">Pets<i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="Cats.php">Cats</a>
+                    <a href="Dogs.php">Dogs</a>
+                  <a href="#">Birds</a>
+    </div>
+  </div>
+              <li><a href="Login.html">Login(Placeholder)</a></li>
+          </ul>
+      </nav> 
+        <main>
+            <h3><i>About us</i></h3> 
+            <p>Welcome to Pet Haven, a platform born out of a shared passion for the welfare and care of animals. Our team is dedicated to connecting like-minded individuals who wish to adopt pets or find loving homes for their own animals. </p>
+        </main>  
+        
+        
+       <?php
+          
+        ?>
+    </body>
+</html>
