22e4c7a7771778862d9e2c6edfc8d9cf37600e4d
Snappy678 <lester1liau@gmail.com>
2024-07-23 12:59:00 +0800

    index.php

--- HTML_PHP/User/index.php
+++ HTML_PHP/User/index.php
@@ -10,18 +10,46 @@ and open the template in the editor.
         <title>Pet Haven</title>
         <link rel="stylesheet" href="style.css">
     </head>
-    <body>
-      <h1>Welcome to Pet Haven</h1>  
+  <body> 
       <nav class="navbar">    
           <ul>
               <li><a href="index.php">Home</a></li>
               <li><a href="AboutUs.php">About Us</a></li>
-              <li><a href="Cats.php">Cats</a></li>
-              <li><a href="Dogs.php">Dogs</a></li>
+              <div class="dropdown">
+                <button class="dropbtn">Pets<i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="Cats.php">Cats</a>
+                    <a href="Dogs.php">Dogs</a>
+                  <a href="#">Birds</a>
+    </div>
+  </div>
+              <li><a href="Login.html">Login(Placeholder)</a></li>
           </ul>
       </nav> 
+      
+      
+      <main>
+          <div class="toppage"></div>
+          
+          <div class="Title1">
+               <h1>PET HAVEN</h1>
+               <p>The Number One Pet Adoption Website</p>
+          </div>
+          <p>Placeholder</p>
+           <p>Please Adopt</p>
+            <p>I'll find you if you don't</p>
+             <p>Respectfully <3</p>
+            
+           
+      </main>      
+      <footer></footer>  
        <?php
           
         ?>
-    </body>
+      
+      <footer>
+          Contact us
+      </footer> 
+  </body>
 </html>
