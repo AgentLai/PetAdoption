2751f4a64b8ce1908d23bdea5fbf729ae182f36f
Snappy678 <lester1liau@gmail.com>
2024-08-15 14:18:53 +0800

    AboutUs.php

--- HTML_PHP/User/AboutUs.php
+++ HTML_PHP/User/AboutUs.php
@@ -9,24 +9,30 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <title>Pet Haven</title>
         <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
     </head>
     <body>
-     <nav class="navbar">    
-          <ul>
-              <li><a href="index.php">Home</a></li>
-              <li><a href="AboutUs.php">About Us</a></li>
-              <div class="dropdown">
-                <button class="dropbtn">Pets<i class="fa fa-caret-down"></i>
-                </button>
-                <div class="dropdown-content">
-                    <a href="Cats.php">Cats</a>
-                    <a href="Dogs.php">Dogs</a>
-                  <a href="#">Birds</a>
-    </div>
-  </div>
-              <li><a href="Login.html">Login(Placeholder)</a></li>
-          </ul>
-      </nav> 
+     <!-- NAVIGATION BAR/HEADER -->  
+    <header> 
+        <div class="nav-container">  
+            <a href ="" class = "logo">Pet Haven</a>  
+            <div class ="navbar">
+                <a href="index.php">Home</a>
+                <a href="AboutUs.php">About Us</a>
+                <a href="Dogs.php">Dogs</a>
+                <a href="Cats.php">Cats</a>
+                <a href="Login.html">Login(Placeholder)</a>
+            </div>
+            <div class = "nav-icons">
+                <a href="Login.php"><i class='bx bx-user'></i></a>
+                <div class="menu-icons">
+                    <div class="line1"></div>
+                    <div class="line2"></div>
+                    <div class="line3"></div>
+                </div>
+            </div>
+        </div>
+    </header> 
         <main>
             <h3><i>About us</i></h3> 
             <p>Welcome to Pet Haven, a platform born out of a shared passion for the welfare and care of animals. Our team is dedicated to connecting like-minded individuals who wish to adopt pets or find loving homes for their own animals. </p>
@@ -36,5 +42,44 @@ and open the template in the editor.
        <?php
           
         ?>
+     <!-- Footer -->
+    <footer class="footer">
+        <div class="footer-container">
+            <div class="footer-row">
+                <div class="footer-col">
+                    <h4>Pet Haven</h4>
+                    <ul>
+                        <li><a href="AboutUs.php">about us</a></li>
+                        <li><a href="#">our services</a></li>
+                        <li><a href="#">privacy policy</a></li>
+                        <li><a href="#"></a></li>
+                    </ul>
+                </div>
+                 <div class="footer-col">
+                    <h4>get help</h4>
+                    <ul>
+                        <li><a href="#">FAQ</a></li>
+                    </ul>
+                </div>
+                 <div class="footer-col">
+                    <h4>adopt now</h4>
+                    <ul>
+                        <li><a href="#">dogs</a></li>
+                        <li><a href="#">cats</a></li>
+                        <li><a href="#">birds</a></li>  
+                    </ul>
+                </div>
+                 <div class="footer-col">
+                    <h4>follow us</h4>
+                    <div class="social-links">
+                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                        <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>      
+    </footer> 
     </body>
 </html>
