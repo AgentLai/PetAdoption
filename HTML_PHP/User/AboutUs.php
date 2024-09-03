d83ea15e04775f696ea998e7c4798df8a8181627
Snappy678 <lester1liau@gmail.com>
2024-09-03 20:46:00 +0800

    Update AboutUs.php

--- HTML_PHP/User/AboutUs.php
+++ HTML_PHP/User/AboutUs.php
@@ -1,85 +1,141 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>Pet Haven</title>
-        <link rel="stylesheet" href="style.css">
-        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
-    </head>
-    <body>
-     <!-- NAVIGATION BAR/HEADER -->  
-    <header> 
-        <div class="nav-container">  
-            <a href ="" class = "logo">Pet Haven</a>  
-            <div class ="navbar">
-                <a href="index.php">Home</a>
-                <a href="AboutUs.php">About Us</a>
-                <a href="Dogs.php">Dogs</a>
-                <a href="Cats.php">Cats</a>
-                <a href="Login.html">Login(Placeholder)</a>
+  <head>
+    <meta charset="UTF-8" />
+    <!-- Use for responsiveness -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- link To CSS -->
+    <link rel="stylesheet" href="style.css" />
+    <!-- link To JS -->
+    <script src="IndexJava.js" defer></script>
+    <!-- For Scroll Reveal -->
+    <script src="https://unpkg.com/scrollreveal"></script>
+    <!-- For Icons -->
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+      integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+    <!-- Link For Gsap -->
+    <script
+      src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js"
+      integrity="sha512-7eHRwcbYkK4d9g/6tD/mhkf++eoTHwpNM9woBxtPUBWm67zeAfFC+HrdoE2GanKeocly/VxeLvIqwvCdk7qScg=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    ></script>
+    <!-- Link For Gsap - Scroll Trigger -->
+    <script
+      src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/ScrollTrigger.min.js"
+      integrity="sha512-onMTRKJBKz8M1TnqqDuGBlowlH0ohFzMXYRNebz+yOcc5TQr/zAKsthzhuv0hiyUKEiQEQXEynnXCvNTOk50dg=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    ></script>
+    <!-- Link For Lenis - Smooth Scrolling -->
+    <script src="https://unpkg.com/@studio-freight/lenis@1.0.42/dist/lenis.min.js"></script>
+    <!-- Link For Split Type -->
+    <script src="https://cdn.jsdelivr.net/npm/split-type@0.3.4/umd/index.min.js"></script>
+
+    <title>Pet Haven |  About Us</title>
+  </head>
+  <body>
+    <nav>
+        <a href="index.html" class ="brand">
+            <h1>Pet<b class="accent">Haven</b></h1>
+        </a>
+        <div class="menu">
+            <div class="btn">
+                <i class = "fas fa-times close-btn"></i>
             </div>
-            <div class = "nav-icons">
-                <a href="Login.php"><i class='bx bx-user'></i></a>
-                <div class="menu-icons">
-                    <div class="line1"></div>
-                    <div class="line2"></div>
-                    <div class="line3"></div>
-                </div>
-            </div>
+            <a href="index.php">Home</a>
+            <a href="AboutUs.php">About Us</a>
+            <a href="Pets.php">Pets</a>
+            <a href="FAQs.php">FAQ's</a>
+            <a href="index.php#stories">Stories</a>
         </div>
-    </header> 
-        <main>
-            <h3><i>About us</i></h3> 
-            <p>Welcome to Pet Haven, a platform born out of a shared passion for the welfare and care of animals. Our team is dedicated to connecting like-minded individuals who wish to adopt pets or find loving homes for their own animals. </p>
-        </main>  
-        
-        
-       <?php
-          
-        ?>
-     <!-- Footer -->
-    <footer class="footer">
-        <div class="footer-container">
-            <div class="footer-row">
-                <div class="footer-col">
-                    <h4>Pet Haven</h4>
-                    <ul>
-                        <li><a href="AboutUs.php">about us</a></li>
-                        <li><a href="#">our services</a></li>
-                        <li><a href="#">privacy policy</a></li>
-                        <li><a href="#"></a></li>
-                    </ul>
-                </div>
-                 <div class="footer-col">
-                    <h4>get help</h4>
-                    <ul>
-                        <li><a href="#">FAQ</a></li>
-                    </ul>
-                </div>
-                 <div class="footer-col">
-                    <h4>adopt now</h4>
-                    <ul>
-                        <li><a href="#">dogs</a></li>
-                        <li><a href="#">cats</a></li>
-                        <li><a href="#">birds</a></li>  
-                    </ul>
-                </div>
-                 <div class="footer-col">
-                    <h4>follow us</h4>
-                    <div class="social-links">
-                        <a href="#"><i class="fab fa-facebook-f"></i></a>
-                        <a href="#"><i class="fab fa-twitter"></i></a>
-                        <a href="#"><i class="fab fa-instagram"></i></a>
-                        <a href="#"><i class="fab fa-linkedin-in"></i></a>
-                    </div>
-                </div>
-            </div>
-        </div>      
-    </footer> 
-    </body>
-</html>
+
+        <button class = "btn-2" onclick="window.location.href='register.php'">
+        <p>Sign Up Now</p>
+        <i class="fa-solid fa-arrow-right-to-bracket"></i>
+        </button>
+
+        <div class="btn">
+            <i class="fas fa-bars menu-btn"></i>
+        </div>
+    </nav>
+
+<div class="box-area">      
+      <div class="MoreAbout-Banner">  
+         <h1>ABOUT US</h1>
+      </div>
+      
+     <div class="MoreAbout">
+        <div class="MoreAbout-wrapper">
+            <h2>Learn More About Us!</h2>
+          <p class="reveal-type" data-bg-color="#cccccc" data-fg-color="black">
+        <b>Pet<span style="color: #87c7af">Haven</span></b> is a sanctuary where the hopes of animals seeking their forever homes intertwine with the dreams of compassionate individuals eager to provide them with the love and care they deserve. 
+        At <b>Pet<span style="color: #87c7af">Haven</span></b>, we believe that every pet is a cherished life waiting for a chance to flourish in a nurturing environment. 
+        <br><br>
+        Our platform serves as a bridge that connects these loving souls to those who are ready to open not just their homes but their hearts, 
+        offering a safe and warm refuge where they can truly belong.</p>
+        <br><br>
+          <h2> Why Pet<span style="color: #87c7af">Haven</span>? </h2>
+        <p class="reveal-type" data-bg-color="#cccccc" data-fg-color="black">   
+       Every year, approximately 7.6 million pets become strays, with about 3.4 million of them being cats in Malaysia alone. 
+       Globally, the World Health Organization (WHO) estimates that there are over 200 million stray dogs.
+       These staggering numbers highlight the urgent need for a solution.
+        </p>
+         <br><br>
+          <h2> Our Mission </h2>
+        <p class="reveal-type" data-bg-color="#cccccc" data-fg-color="black">   
+            At <b>Pet<span style="color: #87c7af">Haven</span></b>, we believe in making a difference by providing a seamless, user-friendly web application to address the stray animal crisis. 
+        Our mission is to reduce the number of abandoned and suffering stray animals by facilitating their adoption.
+        </p>
+      </div>
+    </div>
+</div>    
+    <!-- Footer Section -->
+ <footer id="footer">
+  <div class="footer-container">
+    <div class="footer-links">
+      <h2>Quick Links</h2>
+      <div class="link-container">
+        <div class="links">
+          <a href="index.php#about">About</a>
+          <a href="index.php#pets">Pets</a>
+        </div>
+        <div class="links">
+          <a href="index.php#requirements">Requirements</a>
+          <a href="index.php#stories">Stories</a>
+          <a href="index.php#footer">Contact Us</a>
+        </div>
+      </div>
+    </div>
+    <div class="footer-brand">
+      <h1>Pet<b class="accent">Haven</b></h1>
+      <p>Find Your Purrfect furry friend Today!</p>
+      <div class="socials">
+        <a href="/"><i class="fa-brands fa-facebook-f"></i></a>
+        <a href="/"><i class="fa-brands fa-tiktok"></i></a>
+        <a href="/"><i class="fa-brands fa-twitter"></i></a>
+        <a href="/"><i class="fa-brands fa-linkedin-in"></i></a>   
+      </div>
+    </div>
+    <div class="footer-contact-info">
+      <div class="contact-info-item">
+        <i class="fa-regular fa-envelope"></i>
+        <p>pethaven@gmail.com</p>
+      </div>
+      <div class="contact-info-item">
+        <i class="fa-solid fa-phone"></i>
+        <p>+60 17-816 3645(Douglas)</p>
+      </div>
+    </div>
+  </div>
+    <p class="copyright">All Rights Reserved to <b>Pet Haven</b></p>
+ </footer>
+<!-- Footer Section End -->
+
+  </body>
+</html>  
