882434c5f50b8786151e8faaf8357a809ec2fd3d
EdwinChin2309 <edwinccw2309@gmail.com>
2024-09-24 10:13:14 +0800

    Update

--- database.sql
+++ database.sql
@@ -6,7 +6,7 @@ CREATE TABLE Member (
     DOB DATE,
     Email VARCHAR(100) UNIQUE NOT NULL,
     Password VARCHAR(255) NOT NULL,
-    Status ENUM('Active','Inactive','Blacklisted') NOT NULL
+    Status ENUM('Active','Inactive','Blacklisted') NOT NULL DEFAULT 'Inactive'
 );
 
 
