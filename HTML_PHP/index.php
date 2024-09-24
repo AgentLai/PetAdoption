bb516e5fa9c0c15720cd5471b652f5c63a28eed8
Snappy678 <lester1liau@gmail.com>
2024-09-24 14:22:28 +0800

    Update index.php

--- HTML_PHP/index.php
+++ HTML_PHP/index.php
@@ -8,10 +8,9 @@
     <!-- Use for responsiveness -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <!-- link To CSS -->
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="../JSAndCSS/style.css" />
     <!-- link To JS -->
-    <script src="IndexJava.js" defer></script>
-    <script src="index_application.js" defer></script>
+    <script src="../JSAndCSS/index.js" defer></script>
     <!-- For Scroll Reveal -->
     <script src="https://unpkg.com/scrollreveal"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/scrollReveal.js/2.0.0/scrollReveal.js"></script>
